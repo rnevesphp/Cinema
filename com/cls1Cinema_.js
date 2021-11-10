76fc95e58a338358c0d05a0e68a852661e45f1ab
Romeu <romeu.esp@gmail.com>
2021-11-11 00:54:35 +0100

    :wrench:

--- com/cls1Cinema_.js
+++ com/cls1Cinema_.js
@@ -8,24 +8,4 @@ class cls1Cinema_ {
       this.Cine = new cls2CineFilial_();
       // Atribuimos a la variable Cine la creación de la clase Cine
    }
-   /*
-   __CreateFrontEnd() {
-      // Array con todos los elementos del frontEnd
-      var d_Array = [
-         ["A", "B", "C",],
-         ["D", "E", "F",],
-         ["G", "H", "I",]
-      ];
-      // Container con el loop y añadiendo las celdas
-      let container = document.getElementById("grid");
-      for (let i of d_Array) {
-         for (let x of i) {
-            let cell = document.createElement("div");
-            cell.innerHTML = x;
-            cell.className = "cell";
-            cell.id = x;
-            container.appendChild(cell)
-         }
-      }
-   }*/
 }
