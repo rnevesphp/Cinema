8df7aaee386efdb34a66d58bb28055bd4f53935b
Romeu Neves <romeu.esp@gmail.com>
2021-11-21 23:51:44 +0100

    :package:

--- com/cls06SalaCine_.js
+++ com/cls06SalaCine_.js
@@ -1,27 +1,19 @@
 class cls06SalaCine_ {
    constructor(numSalaID, numAsientosSala) {
-      this.numSalaID = numSalaID; // ID de la sala creado en la clase CineFilial
-      this.numAsientosSala = numAsientosSala; // Numero de asientos por sala en la clase CineFilial
-
-      this.Seats = []; // Array para almacenar cada uno de los asientos
-      this.containerHTML = document.getElementById("grid_sala"); // cogemos el container del doc HTML 
-      this.CreateAsientos(); // Llamamos el metodo que que crea los asientos
-
+      this.numSalaID = numSalaID;
+      this.numAsientosSala = numAsientosSala;
+      this.Seats = [];
+      this.containerHTML = document.getElementById("grid_sala");
+      this.CreateAsientos();
       this.CreateCeldaSala();
       this.celdaSala;
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
    CreateAsientos() {
-      for (var i = 0; i < this.numAsientosSala; i++) { // para la  variable i que es menor que el numero 
-         // de asientos por sala entonces se incrementará hasta que tenga los mismos valores
-         var cAsientos = new cls07Asientos_(i); // creamos una nueva clase para los asientos y le añadimos a la variable 
-         // cAsientos
-         this.Seats.push(cAsientos); // introducimos dentro del array el valor de la nueva clase 
-         //con la variable cAsientos 
+      for (var i = 0; i < this.numAsientosSala; i++) {
+         var cAsientos = new cls07Asientos_(i);
+         this.Seats.push(cAsientos);
       }
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
-   /**/
    CreateCeldaSala() {
       this.celdaSala = document.createElement("div");
       this.celdaSala.innerHTML = "Sala: " + this.numSalaID;
@@ -29,18 +21,16 @@ class cls06SalaCine_ {
       this.celdaSala.addEventListener("click", this.onclick.bind(this));
       this.celdaSala.id = "Sala " + this.numSalaID;
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
-   /**/
    DrawUniqueSala() {
       this.containerHTML.appendChild(this.celdaSala);
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
-   /**/
    onclick() {
       this.EraseAsientos();
       this.drawAsientos();
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
+   EraseAsientos() {
+      this.containerHTML.innerHTML = "";
+   }
    drawAsientos() {
       for (var i = 0; i < this.numAsientosSala; i++) {
          var cAsientos = this.Seats[i];
@@ -48,8 +38,5 @@ class cls06SalaCine_ {
          this.containerHTML.appendChild(tCell);
       }
    }
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
-   EraseAsientos() {
-      this.containerHTML.innerHTML = "";
-   }
+
 }
--- com/cls07Asientos_.js
+++ com/cls07Asientos_.js
@@ -4,35 +4,27 @@ class cls07Asientos_ {
       this._asientoOcu = 0; //Math.round(Math.random()); // ocupación del asiento en la sala
       this.celdaHTML;
 
-      this._Inicio();
-   }
-   //////////////////////////////////////////////////////
-   _Inicio() {
       this._CrearCell();
    }
-   //////////////////////////////////////////////////////
    _CrearCell() {
       this.celdaHTML = document.createElement("div") // creación de la s divs en el documento HTML 
       this.celdaHTML.innerHTML = "Asiento: " + this._numAs;
       this.celdaHTML.addEventListener("click", this.onclick.bind(this));
       this.celdaHTML.id = "A-" + this._numAs;
    }
-   //////////////////////////////////////////////////////
    ReservarAsiento() {
       console.log("Asiento escogido");
       this._asientoOcu = 1;
       this.celdaHTML.removeEventListener("click", null, null);
       this.Draw();
    }
-   //////////////////////////////////////////////////////
    Draw() {
       this.celdaHTML.className = this._getClassColor();
       return this.celdaHTML;
    }
-
    _getClassColor() {
       var tClass = "";
-      if (this.AsOcupado()) { // asiento
+      if (this.AsOcupado()) {
          tClass = "cell_red";
       } else {
          tClass = "cell";
@@ -42,11 +34,9 @@ class cls07Asientos_ {
       }
       return tClass;
    }
-
    AsOcupado() {
       return (this._asientoOcu > 0);
    }
-
    onclick() {
       localStorage.setItem(this._numAs, 1);
       if (this._asientoOcu) {
